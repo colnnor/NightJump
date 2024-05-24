525182ed19771e5228939bf4d6897325c80e4623
colnnor <135847463+colnnor@users.noreply.github.com>
2024-05-23 21:53:12 -0400

    Updated main menu

--- _Project/_Scripts/Managers/MainMenuManager.cs
+++ _Project/_Scripts/Managers/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using MoreMountains.Feedbacks;
 using Sirenix.OdinInspector;
 using System;
@@ -11,21 +12,29 @@ public class MainMenuManager : MonoBehaviour
 
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject settingsPanel;
-    [SerializeField] private GameObject confirmQuitPanel;
+    [SerializeField] private GameObject gameOverMenu;
+
+    [SerializeField] private float animTime;
+    [SerializeField] private Ease easeType = Ease.InOutQuart;
+
     [SerializeField] private EventChannel sceneLoaded;
 
     [Title("Feedbacks")]
     [SerializeField] private MMF_Player feedbacks;
 
-    private Vector3 center = new(0, 0, 0);
-    private Vector3 left = new(-1920, 0, 0);
-    private Vector3 right = new(1920, 0, 0);
-    private Vector3 top = new(0, 1080, 0);
+    private Dictionary<MenuState, GameObject> menuScales;
 
     private void Awake()
     {
         ServiceLocator.Instance.RegisterService<MainMenuManager>(this);
+        menuScales = new()
+        {
+            { MenuState.Main, mainPanel },
+            { MenuState.Settings, settingsPanel },
+            { MenuState.GameOver, gameOverMenu },
+        };
     }
+
 
     private void OnDestroy()
     {
@@ -36,6 +45,7 @@ public class MainMenuManager : MonoBehaviour
         SetMenuState(MenuState.Main);
         sceneLoaded?.Invoke(new Empty());
     }
+
     public void UIClick()
     {
         AudioManager.Instance.PlayOneShot(SFXType.ButtonClickPositive, randomPitch: true);
@@ -46,41 +56,30 @@ public class MainMenuManager : MonoBehaviour
         Play?.Invoke();
     }
 
-    public void SetMenuState(MenuState state)
-    {
-        settingsPanel.SetActive(state == MenuState.Settings);
-        confirmQuitPanel.SetActive(state == MenuState.GameOver);
-        mainPanel.SetActive(state == MenuState.Main);
-    }
 
-    public void ShowMainPanel() => SetMenuState(MenuState.Main);
-    public void ShowSettings() => SetMenuState(MenuState.Settings);
-    public void ShowConfirmQuitPanel() => SetMenuState(MenuState.GameOver);
+    public void ShowMainPanel() => SetMenuState(MenuState.Main, animTime);
+    public void ShowSettings() => SetMenuState(MenuState.Settings, animTime);
+    public void ShowConfirmQuitPanel() => SetMenuState(MenuState.GameOver, animTime);
 
+    private void SetMenuState(MenuState state, float? animTime = null)
+    {
+        float animationTime = animTime ?? this.animTime;
 
-    #region manuals
-    [Button]
-    void ManualMainPage()
-    {
-        mainPanel.transform.localPosition = center;
-        settingsPanel.transform.localPosition = Vector3.zero.With(x: 1920);
-        confirmQuitPanel.transform.localPosition = Vector3.zero.With(y: -1080);
+        foreach (var menu in menuScales)
+        {
+            if (!menu.Value.activeSelf)
+                return;
+
+            if (menu.Key == state)
+            {
+                menu.Value.transform.DOScale(Vector3.one, animationTime).SetEase(easeType);
+            }
+            else
+            {
+                menu.Value.transform.DOScale(Vector3.zero, animationTime).SetEase(easeType);
+            }
+        }
     }
-    [Button]
-    void ManualSettingsPage()
-    {
-        mainPanel.transform.localPosition = Vector3.zero.With(x: -1920);
-        settingsPanel.transform.localPosition = center;
-        confirmQuitPanel.transform.localPosition = Vector3.zero.With(y: -1080);
-    }
-    [Button]
-    void ManualConfirmQuitPage()
-    {
-        mainPanel.transform.localPosition = Vector3.zero.With(x: -1920);
-        settingsPanel.transform.localPosition = Vector3.zero.With(x: 1920);
-        confirmQuitPanel.transform.localPosition = center;
-    }
-    #endregion
 }
 
 public enum ContainerAnimation
